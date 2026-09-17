e305e347551f32af80ab7def2e2b1d9d9d652f10
jericizon <im.jericizon@gmail.com>
2026-09-17 22:32:04 +0800

    fix(homepage): drop duplicate live weather panel from Santa Rosa Today chapter

--- pages/index.vue
+++ pages/index.vue
@@ -329,7 +329,6 @@ function revealClass(isVisible: boolean) {
             :suffix="stat.suffix"
           />
         </div>
-        <CivicWeatherToday class="mt-12" />
       </div>
     </section>
 
